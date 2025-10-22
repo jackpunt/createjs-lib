4de65369f47abb14f7c32f875c81e447f11b1105
jackpunt <jpeckj@gmail.com>
2025-10-22 14:23:11 -0700

    interim refactor: PageMaker & LayoutSpec

--- src/image-grid.ts
+++ src/image-grid.ts
@@ -2,14 +2,32 @@ import { stime, WH } from "@thegraid/common-lib";
 import { Container, DisplayObject, Stage } from "@thegraid/easeljs-module";
 import { makeStage } from "./createjs-functions";
 import { RectShape } from "./paintable";
+import { CenterText } from "./center-text";
+import { NamedContainer } from "./named-container";
 
-
-export type GridSpec = {
+/** basic fields for layout of graphics on template */
+export type LayoutSpec = {
   /** pixel width of png template */
   width: number,  // canvas size
   /** pixel height of png template */
   height: number, // canvas size
+  /** amount to extend card image beyond cardw & cardh; (typically: .1 inch, 25-30 mm) */
+  bleed?: number,
+  /** if defined, paint a bg RectShape behind the ImageGrid */
+  bgColor?: string,
+  /** set scale on View canvas [.1] */
+  scale?: number,
+}
+
+/** layout for grid of graphics for print & punch */
+export type GridSpec = LayoutSpec & {
+  /** pixel width of png template */
+  width: number,  // canvas size
+  /** pixel height of png template */
+  height: number, // canvas size
+  /** number of rows in grid */
   nrow: number,
+  /** number of items per row */
   ncol: number,
   /** top margin 'indent' to center of card */
   y0: number,
@@ -24,8 +42,6 @@ export type GridSpec = {
   cardw?: number,
   /** cut size of card image on template (blue lines) [cardh <= dely]; */
   cardh?: number,
-  /** amount to extend card image beyond cardw & cardh; (typically: .1 inch, 25-30 mm) */
-  bleed?: number,
   /** for close-packed shapes, exclude bleed on Central edges, include on L & R */
   trimLCR?: boolean,
   /** [1: already in pixels] scale factor for [x0/x1, y0, delx, dely] --> pixels */
@@ -36,23 +52,209 @@ export type GridSpec = {
   split?:  boolean,
   /** set true or false to override orientation derived from delx/dely */
   land?: boolean,
-  /** if defined, paint a bg RectShape behind the ImageGrid */
-  bgColor?: string,
-  /** set scale on View canvas [.1] */
-  scale?: number,
 }
 
 export type PageSpec = {
-  gridSpec: GridSpec,
-  frontObjs: DisplayObject[],
-  backObjs?: (DisplayObject  | undefined)[] | undefined,
+  layoutSpec?: LayoutSpec, // can use: gridSpec = pageSpec.layoutSpec as GridSpec
+  gridSpec?: GridSpec,     // retain for original/legacy TileExporter apps
+  frontObjs?: DisplayObject[], // for gridSpec
+  backObjs?: (DisplayObject  | undefined)[] | undefined, // for gridSpec.double
   canvas?: HTMLCanvasElement,
   basename?: string,
 }
 
+/** make canvas pages for previewing & download for print */
+export class PageMaker {
+  constructor(makePageSpecs: () => PageSpec[], buttonId = 'makePage', label = 'MakePages') {
+    this.setAnchorClick(buttonId, label, () => {
+      this.setAnchorClick(buttonId, 'Making...')
+      setTimeout(() => {
+        const pageSpecs = makePageSpecs();
+        this.downloadPageSpecs(pageSpecs);
+        this.setAnchorClick(buttonId, label)
+      }, 0);
+    });
+    this.setAnchorClick('scalePlus', '+', () => this.scalePlus())
+    this.setAnchorClick('scaleMinus', '-', () => this.scaleMinus())
+  }
+
+  /** stage for addObjects */
+  stage!: Stage;
+  /** draw objects on this canvas, retain in pageSpec.canvas */
+  canvas!: HTMLCanvasElement;
+
+  scales = ['0.05', '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.75'];
+
+  scalePlus() {
+    const canvasDiv = document.getElementById('canvasDiv') as HTMLCanvasElement;
+    const scale = canvasDiv.style.scale;
+    const nScale = this.scales.find(scal => scal > scale) ?? scale;
+    canvasDiv.style.setProperty('scale', nScale);
+  }
+  scaleMinus() {
+    const canvasDiv = document.getElementById('canvasDiv') as HTMLCanvasElement;
+    const scale = canvasDiv.style.scale;
+    const rscales = this.scales.slice().reverse();
+    const nScale = rscales.find(scal => scal < scale) ?? scale;
+    canvasDiv.style.setProperty('scale', nScale);
+  }
+
+  setScale(newScale?: string) {
+    const canvasDiv = document.getElementById('canvasDiv') as HTMLCanvasElement;
+    canvasDiv.style.setProperty('scale', newScale);
+  }
+
+  /** 
+   * create a stage on given canvas, for addObjects()
+   * @param wh setCanvasSize(wh)
+   * @param canvasId ['gridCanvas'] any string or an HTMLCanvasElement; suitable for new Stage()
+   * @param scale [.2] canvasDiv.style.setProperty('scale', `${scale}`)
+   */
+  setStageAndCanvas(wh: WH, canvasId: string | HTMLCanvasElement = 'gridCanvas', scale = .2) {
+    if (typeof canvasId === 'string') {
+      this.canvas = (document.getElementById(canvasId) ?? document.createElement('canvas')) as HTMLCanvasElement;
+      this.canvas.id = canvasId;
+    } else {
+      this.canvas = canvasId as HTMLCanvasElement;
+    }
+    const canvasDiv = document.getElementById('canvasDiv') as HTMLCanvasElement;
+    canvasDiv.style.setProperty('scale', `${scale}`);
+    this.stage = makeStage(this.canvas, false);
+    this.stage.removeAllChildren();
+    this.setCanvasSize(wh);
+  }
+
+  setCanvasSize(wh: WH) {
+    this.canvas.width = wh.width;
+    this.canvas.height = wh.height;
+  }
+
+  setAnchorClick(id: string, label: string, onclick?: ((ev: MouseEvent) => void) | 'stop') {
+    const anchor = document.getElementById(id) as HTMLAnchorElement;
+    anchor.innerHTML = `<button type="button">${label}</button>`;
+    if (onclick === 'stop') { anchor.onclick = null; }
+    else if (onclick) { anchor.onclick = onclick }
+  }
+
+  downloadPageSpecs(pageSpecs: PageSpec[]) {
+    const baseName = (n=0) => `P${n}${pageSpecs[n].basename ? `_${pageSpecs[n].basename}` : ''}`;
+
+    // step thru downloading each canvas.toDataURL() image:
+    let downClick = 0;             // next image to download
+    const downloadPage = (n: number) => {
+      const pageSpec = pageSpecs[n];
+      const canvas = pageSpec.canvas as HTMLCanvasElement;
+      const base = baseName(n);
+      const filename = `${base}@${stime.fs("MM-DD_kk_mm_ssL")}.png`;
+      // console.log(stime(this, `.downloadClick: ${canvasId} -> ${filename}`))
+      this.downloadImage(canvas, filename);
+      const next = `${(downClick < pageSpecs.length) ? baseName(downClick): 'done'}`
+      this.setAnchorClick('download', `Download ${next}`);
+    }
+
+    const setDownload = (n: number) => {
+      downClick = n;
+      this.setAnchorClick('download', `Download ${baseName(downClick)}`, (ev) => {
+        if (downClick >= pageSpecs.length) {
+          this.addCanvas(undefined);
+          this.setAnchorClick('download', 'Download done', 'stop');
+          return;
+        }
+        downloadPage(downClick++);
+      });
+    }
+
+    // step thru viewing each canvas/image:
+    let viewClick = 0;
+    const viewPage = (n = viewClick) => {
+      setDownload(n);        // "what you see is what you get"
+      const pageSpec = pageSpecs[n];
+      const canvas = pageSpec.canvas as HTMLCanvasElement;
+      canvas.style.border = "2px solid";
+      this.addCanvas(canvas);
+      viewClick = n + 1;
+      const next = `${(viewClick < pageSpecs.length) ? `P${viewClick}`: 'done'}`
+      this.setAnchorClick('viewPage', `View Next: ${next}`, () => {
+        if (viewClick < pageSpecs.length) {
+          viewPage();
+        } else {
+          this.addCanvas(undefined);
+          this.setAnchorClick('viewPage', 'View Next: done', 'stop');
+        }
+      })
+    }
+
+    this.setAnchorClick('viewPage0', 'View P0', () => viewPage(viewClick = 0));
+    viewPage(viewClick = 0);   // includes: setDownload(0);
+    return;
+  }
+
+  /** Id of current canvas to view */
+  canvasId: string | undefined;
+  /** add canvas to 'canvasDiv' */
+  addCanvas(canvas: HTMLCanvasElement | undefined) {
+    const div = document.getElementById('canvasDiv') as HTMLDivElement;
+    if (this.canvasId) {
+      const elt = document.getElementById(this.canvasId);
+      elt?.parentNode?.removeChild(elt);
+      this.canvasId = undefined;
+    }
+    if (canvas) {
+      this.canvasId = canvas.id;
+      div.appendChild(canvas);
+    }
+  }
+
+  downloadImage(canvas: HTMLCanvasElement, filename = 'image.png', downloadId = 'download') {
+    const anchor = document.getElementById(downloadId) as HTMLAnchorElement;
+    const imageURL = canvas.toDataURL("image/png");
+    const octetURL = imageURL.replace("image/png", "image/octet-stream");
+    anchor.download = filename;
+    anchor.href = octetURL;
+    console.log(stime(this, `.downloadImage: ${canvas.id} -> ${filename} ${octetURL.length}`))
+  }  
+
+  /** 
+   * Make a single page image, injecting the canvas into given pageSpec.
+   * 
+   * Make a Stage on canvas (maybe create from canvasId); 
+   * fill background then place objects using gridSpec.
+   * 
+   * Set pageSpec.canvas with completed HTMLCanvasElement.
+   * 
+   * @param pageSpec \{ gridSpec, frontObjs, backObjs? }
+   * @param canvas a Canvas or canvasId
+   * @returns (pageSpec.canvas holding the page image)
+   */
+  makePage(pageSpec: PageSpec, canvas?: HTMLCanvasElement | string ) {
+    const gridSpec = pageSpec.gridSpec, { bgColor, scale } = gridSpec;
+    const width = gridSpec.width * (gridSpec.dpi ?? 1);
+    const height = gridSpec.height * (gridSpec.dpi ?? 1);
+    this.setStageAndCanvas({ width, height }, canvas, scale);
+    if (bgColor) {
+      const bg = new RectShape({ x: 0, y: 0, w: width, h: height }, bgColor, '')
+      this.stage.addChild(bg)
+    }
+    const nc = this.addObjects(pageSpec)
+    this.stage.update();
+    pageSpec.canvas = this.canvas;
+
+    const { id } = this.canvas;
+    const info = { id, width, height, nc }; // not essential...
+    console.log(stime(this, `.makePage: info =`), info);
+    return pageSpec;
+  }
+
+  /** override this abstract method to add  */
+  addObjects(pageSpec: PageSpec) {
+    const cont = new NamedContainer('default', this.canvas.width / 2, this.canvas.height / 2);
+    cont.addChild(new CenterText('override addObjects() to see page contents'))
+    this.stage.addChild(cont);
+  }
+}
 
 /** Setup html buttons, manage canvases, and add "Tile" to grid based on GridSpec. */
-export class ImageGrid {
+export class ImageGrid extends PageMaker {
   // Office Depot stick-on circles; on Brother HL-L3210CW printer
   static circle_1_inch: GridSpec = {
     width: 8.433, height: 10.967, // not quite 8.5 X 11.0
@@ -120,97 +322,15 @@ export class ImageGrid {
     width: 3600, height: 5400, nrow: 3, ncol: 5, cardh: 525, cardw: 750, double: true,
     x0: 334 + 1.75 * 150   , y0: 150 + 2.5 * 150, delx: 600, dely: 825, bleed: 30,  // (2705-305)/4, (1770-120)/2
   }
-  stage!: Stage;
-  canvas!: HTMLCanvasElement;
 
-  constructor(makePageSpecs: () => PageSpec[], buttonId = 'makePage', label = 'MakePages') {
-    this.setAnchorClick(buttonId, label, () => {
-      this.setAnchorClick(buttonId, 'Making...')
-      setTimeout(() => {
-        this.downloadPageSpecs(makePageSpecs());
-        this.setAnchorClick(buttonId, label)
-      }, 0);
-    });
-    this.setAnchorClick('scalePlus', '+', () => this.scalePlus())
-    this.setAnchorClick('scaleMinus', '-', () => this.scaleMinus())
-  }
-
-  scales = ['0.05', '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.75'];
-
-  scalePlus() {
-    const canvasDiv = document.getElementById('canvasDiv') as HTMLCanvasElement;
-    const scale = canvasDiv.style.scale;
-    const nScale = this.scales.find(scal => scal > scale) ?? scale;
-    canvasDiv.style.setProperty('scale', nScale);
-  }
-  scaleMinus() {
-    const canvasDiv = document.getElementById('canvasDiv') as HTMLCanvasElement;
-    const scale = canvasDiv.style.scale;
-    const rscales = this.scales.slice().reverse();
-    const nScale = rscales.find(scal => scal < scale) ?? scale;
-    canvasDiv.style.setProperty('scale', nScale);
-  }
-
-  setScale(newScale?: string) {
-    const canvasDiv = document.getElementById('canvasDiv') as HTMLCanvasElement;
-    canvasDiv.style.setProperty('scale', newScale);
-  }
-
-  setStageAndCanvas(wh: WH, canvasId: string | HTMLCanvasElement = 'gridCanvas', scale = .25) {
-    if (typeof canvasId === 'string') {
-      this.canvas = (document.getElementById(canvasId) ?? document.createElement('canvas')) as HTMLCanvasElement;
-      this.canvas.id = canvasId;
-    } else {
-      this.canvas = canvasId as HTMLCanvasElement;
-    }
-    const canvasDiv = document.getElementById('canvasDiv') as HTMLCanvasElement;
-    canvasDiv.style.setProperty('scale', `${scale}`);
-    this.stage = makeStage(this.canvas, false);
-    this.stage.removeAllChildren();
-    this.setCanvasSize(wh);
-  }
-
-  setCanvasSize(wh: WH) {
-    this.canvas.width = wh.width;
-    this.canvas.height = wh.height;
-  }
-
-  /** 
-   * Make a single page image, injecting the canvas into given pageSpec.
-   * 
-   * Make a Stage on canvas (maybe create from canvasId); 
-   * fill background then place objects using gridSpec.
-   * 
-   * Set pageSpec.canvas with completed HTMLCanvasElement.
-   * 
-   * @param pageSpec \{ gridSpec, frontObjs, backObjs? }
-   * @param canvas a Canvas or canvasId
-   * @returns (pageSpec.canvas holding the page image)
-   */
-  makePage(pageSpec: PageSpec, canvas?: HTMLCanvasElement | string ) {
-    const gridSpec = pageSpec.gridSpec, { bgColor, scale } = gridSpec;
-    const width = gridSpec.width * (gridSpec.dpi ?? 1);
-    const height = gridSpec.height * (gridSpec.dpi ?? 1);
-    this.setStageAndCanvas({ width, height }, canvas, scale);
-    if (bgColor) {
-      const bg = new RectShape({ x: 0, y: 0, w: width, h: height }, bgColor, '')
-      this.stage.addChild(bg)
-    }
-    const nc = this.addObjects(gridSpec, pageSpec.frontObjs, pageSpec.backObjs)
-    this.stage.update();
-    pageSpec.canvas = this.canvas;
-
-    const { id } = this.canvas;
-    const info = { id, width, height, nc }; // not essential...
-    console.log(stime(this, `.makePage: info =`), info);
-    return;
-  }
-
-  /** place objects (front & back) on stage, filling rows by column, until nrows filled. 
+  /** place objects (front & back) on this.stage, filling rows by column, until nrows filled. 
    * 
    * @returns number of child ojects added
    */
-  addObjects(gridSpec: GridSpec, frontObjs: DisplayObject[], backObjs: (DisplayObject | undefined)[] | undefined) {
+  override addObjects(pageSpec: PageSpec) {
+    const gridSpec: GridSpec = pageSpec.gridSpec; 
+    const frontObjs: DisplayObject[] = pageSpec.frontObjs; 
+    const backObjs: (DisplayObject | undefined)[] | undefined = pageSpec.backObjs;
     const cont = new Container();
     const def = { x0: 0, y0: 0, delx: 300, dely: 300, dpi: 1 }
     const { width, height, x0, y0, x1, delx, dely, dpi, nrow, ncol } = { ...def, ...gridSpec };
@@ -236,88 +356,5 @@ export class ImageGrid {
       }
     });
     return cont.numChildren;
-  }
-
-  setAnchorClick(id: string, label: string, onclick?: ((ev: MouseEvent) => void) | 'stop') {
-    const anchor = document.getElementById(id) as HTMLAnchorElement;
-    anchor.innerHTML = `<button type="button">${label}</button>`;
-    if (onclick === 'stop') { anchor.onclick = null; }
-    else if (onclick) { anchor.onclick = onclick }
-  }
-
-  downloadPageSpecs(pageSpecs: PageSpec[]) {
-    const baseName = (n=0) => `P${n}${pageSpecs[n].basename ? `_${pageSpecs[n].basename}` : ''}`;
-
-    // step thru downloading each canvas.toDataURL() image:
-    let downClick = 0;             // next image to download
-    const downloadPage = (n: number) => {
-      const pageSpec = pageSpecs[n];
-      const canvas = pageSpec.canvas as HTMLCanvasElement;
-      const base = baseName(n);
-      const filename = `${base}@${stime.fs("MM-DD_kk_mm_ssL")}.png`;
-      // console.log(stime(this, `.downloadClick: ${canvasId} -> ${filename}`))
-      this.downloadImage(canvas, filename);
-      const next = `${(downClick < pageSpecs.length) ? baseName(downClick): 'done'}`
-      this.setAnchorClick('download', `Download ${next}`);
-    }
-
-    const setDownload = (n: number) => {
-      downClick = n;
-      this.setAnchorClick('download', `Download ${baseName(downClick)}`, (ev) => {
-        if (downClick >= pageSpecs.length) {
-          this.addCanvas(undefined);
-          this.setAnchorClick('download', 'Download done', 'stop');
-          return;
-        }
-        downloadPage(downClick++);
-      });
-    }
-
-    // step thru viewing each canvas/image:
-    let viewClick = 0;
-    const viewPage = (n = viewClick) => {
-      setDownload(n);        // "what you see is what you get"
-      const pageSpec = pageSpecs[n];
-      const canvas = pageSpec.canvas as HTMLCanvasElement;
-      canvas.style.border = "2px solid";
-      this.addCanvas(canvas);
-      viewClick = n + 1;
-      const next = `${(viewClick < pageSpecs.length) ? `P${viewClick}`: 'done'}`
-      this.setAnchorClick('viewPage', `View Next: ${next}`, () => {
-        if (viewClick < pageSpecs.length) {
-          viewPage();
-        } else {
-          this.addCanvas(undefined);
-          this.setAnchorClick('viewPage', 'View Next: done', 'stop');
-        }
-      })
-    }
-
-    this.setAnchorClick('viewPage0', 'View P0', () => viewPage(viewClick = 0));
-    viewPage(viewClick = 0);   // includes: setDownload(0);
-    return;
-  }
-
-  canvasId: string | undefined;
-  addCanvas(canvas: HTMLCanvasElement | undefined) {
-    const div = document.getElementById('canvasDiv') as HTMLDivElement;
-    if (this.canvasId) {
-      const elt = document.getElementById(this.canvasId);
-      elt?.parentNode?.removeChild(elt);
-      this.canvasId = undefined;
-    }
-    if (canvas) {
-      this.canvasId = canvas.id;
-      div.appendChild(canvas);
-    }
-  }
-
-  downloadImage(canvas: HTMLCanvasElement, filename = 'image.png', downloadId = 'download') {
-    const anchor = document.getElementById(downloadId) as HTMLAnchorElement;
-    const imageURL = canvas.toDataURL("image/png");
-    const octetURL = imageURL.replace("image/png", "image/octet-stream");
-    anchor.download = filename;
-    anchor.href = octetURL;
-    console.log(stime(this, `.downloadImage: ${canvas.id} -> ${filename} ${octetURL.length}`))
   }
 }
